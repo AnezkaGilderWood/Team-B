d2f43470655e0e932a957a75716ccdb2af36b88c
Anusheh02 <116880908+Anusheh02@users.noreply.github.com>
2022-11-11 16:06:04 +0000

    minor syntax and error fixes
    now need to edit so it can run through an array of x values

--- wf input.py
+++ wf input.py
@@ -1,14 +1,13 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 11 15:35:30 2022
+Created on Fri Nov 11 14:33:06 2022
 
-@author: zjap015
+@author: Anusheh & Anezka
 """
-
 import numpy as np
 class Schrodinger:
-    
-    def psi:
+    def psi(x):
         """
         function to return a wave function
         this function has some built in formats for the wave function these include:
@@ -22,11 +21,11 @@ class Schrodinger:
         """
         
         print('In this function we have some pre-defined wavefunction formats available to use: ')
-        print('1) ax + b /n * 2) ax**2 + bx + c /n * 3) acos(x) + ibsin(x) /n * 4) acos(x) + bsin(x) /n * 5) ax**b ')
+        print(' y1) ax + b \n 2) ax**2 + bx + c \n 3) acos(x) + ibsin(x) \n 4) acos(x) + bsin(x) \n 5) ax**b ')
               
         wf_yn = input('would you like to use one of our pre defined wave function formats: yes [y] or no [n] ')
         if wf_yn == 'y' or wf_yn == yes:
-            wf= input ('number wave function format would you like to use: ')
+            wf= int(input ('number wave function format would you like to use: '))
             if wf == 1:
                 a = int(input('enter a value: '))
                 b = int(input('enter b value: '))
@@ -47,12 +46,12 @@ class Schrodinger:
             elif wf == 4:
                 a = int(input('enter a value: '))
                 b = int(input('enter b value: '))
-                psi =a*np.cos(x) + b*np.sin(x)
+                psi = a*np.cos(x) + b*np.sin(x)
                 
             elif wf == 5:
                 a = int(input('enter a value: '))
                 b = int(input('enter b value: '))
-                psi =a*x**b 
+                psi = a*x**(b) 
             
             else: 
                 print ('error with selection enter wave function manually')
@@ -61,4 +60,14 @@ class Schrodinger:
         else:
             psi = input('enter the custom wave function you would like: ')
         
-       return psi
+        return psi
+
+x = 1
+Schrodinger.psi(x)
+        
+        
+        
+         
+         
+         
+         
