5ad2849936c8567e7206ba565d10b7990d2320f2
Anusheh02 <116880908+Anusheh02@users.noreply.github.com>
2022-11-11 15:12:20 +0000

    wf input
    this is not finished nez is set to finish

---
+++ wf input.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 11 14:33:06 2022
+
+@author: anusheh
+"""
+import numpy as np
+class Schrodinger:
+    
+    def psi(x):
+        """
+        doc holliday
+        """
+        
+        print('In this function we have some pre-defined wavefunction formats available to use: )
+        print('1) ax + b /n * 2) ax**2 + bx + c /n * 3) acos(x) + ibsin(x) /n * 4) acos(x) + bsin(x) /n * 5) ax**b ')
+              
+        wf_yn = input('would you like to use one of our pre defined wave function formats: yes [y] or no [n] ')
+        if wf_yn == 'y' or wf_yn == yes:
+            wf= input ('number wave function format would you like to use: ')
+            if wf == 1:
+                a = 
+                b =
+                psi =
+                
+            elif wf == 2:
+            
+            elif wf == 3:
+            
+            elif wf == 4:
+            
+            elif wf == 5:
+            
+            else: 
+                print ('error with selection enter wave function manually')
+                psi = input('enter the custom wave function you would like: ')
+        
+        else:
+            psi = input('enter the custom wave function you would like: ')
+        
+       return psi 
+        
+        
+        
+        
+         
+         
+         
+         
